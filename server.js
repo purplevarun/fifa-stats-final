3acbbb0dfd23f1ccdcc438d41bad51127ff729e5
purplevarun <purplevarun@gmail.com>
2021-11-06 06:06:55 +0530

    files

--- server.js
+++ server.js
@@ -2,16 +2,21 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+// config
 dotenv.config();
 const mongourl = process.env.mongourl;
 const port = process.env.PORT || 3000;
-// config
 const app = express();
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 mongoose.connect(mongourl, (err) => {
   if (err) throw err;
   console.log("connected to cloud database");
 });
 // routes
+app.use("/", require("./routes/index"));
 // server initiation
 app.listen(port, (err) => {
   if (err) throw err;

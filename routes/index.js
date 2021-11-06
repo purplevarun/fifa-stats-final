75d486e5339f437054f3b8533fba1bb600254d78
purplevarun <purplevarun@gmail.com>
2021-11-06 07:53:19 +0530

    getting data

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,11 @@ const router = express.Router();
 router.get("/add", (req, res) => {
   res.render("addPlayer");
 });
+router.post("/add/player", (req, res) => {
+  const data = req.body;
+  console.log(data);
+  res.redirect("back");
+});
 router.get("/", (req, res) => {
   res.render("homepage");
 });

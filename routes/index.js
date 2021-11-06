c322c1abca48e2405cc4ae4a7cbbbfdd3a4d4d03
purplevarun <purplevarun@gmail.com>
2021-11-06 07:11:20 +0530

    deafult body page created for ease

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+
+router.get("/add", (req, res) => {
+  res.render("addPlayer");
+});
 router.get("/", (req, res) => {
   res.render("homepage");
 });

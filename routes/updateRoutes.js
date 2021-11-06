7beeafcd23d4a0ab75a452d4c0be59c7b93413e3
purplevarun <purplevarun@gmail.com>
2021-11-06 09:51:11 +0530

    two kinds of dropdowns

--- routes/updateRoutes.js
+++ routes/updateRoutes.js
@@ -4,7 +4,15 @@ const router = express.Router();
 const playerTotal = require("../models/playerTotal");
 const playerSeason = require("../models/playerSeason");
 //
-router.get("/choose", (req, res) => {
+router.get("/choose/sortby/:sorter", (req, res) => {
+  const sorter = {};
+  sorter[req.params.sorter] = "desc";
+  playerTotal
+    .find({})
+    .sort(sorter)
+    .exec((err, doc) => {
+      console.log(doc);
+    });
   res.render("choosePlayerForUpdate");
 });
 module.exports = router;
